276c8877a165f110d9ca32308c0c533050e2b75e
peckaf <maksimbelza8@gmail.com>
2025-02-09 18:25:02 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,9 +1,9 @@
 while wait(0.5) do
-	for i, beer in ipairs(workspace:GetDescendants()) do
-		if beer:FindFirstChild("Humanoid",beer) then
-			if not beer:FindFirstChild("EspBox") then
-				if beer ~= game.Players.LocalPlayer.Character then	
-					local esp = instance.new("BoxHandleAdornment",beer)
+	for i, childrik in ipairs(workspace:GetDescendants()) do
+		if childrik:FindFirstChild("Humanoid",childrik) then
+			if not childrik:FindFirstChild("EspBox") then
+				if childrik ~= game.Players.LocalPlayer.Character then	
+					local esp = instance.new("BoxHandleAdornment",childrik)
 					esp.Adornee = beer
 					esp.ZIndex = 0
 					esp.Size = Vector3.new(4, 5,1)
@@ -11,8 +11,8 @@ while wait(0.5) do
 					esp.Color3 = Color3.fromRGB(238,59,59)
 					esp.AlwaysOnTop  = true
 					esp.Name = "EspBox"
-			    end
+			    	end
 			end	
 		end	
-    end
- 
+   	 end
+end
